c2b8f7377f7e8a831b6f5524efe99ac660af29ba
Kacper Siemaszko <32499287+Siemashko@users.noreply.github.com>
2021-03-15 12:36:10 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from Bio import SeqIO
+
+def gc_content(sequence):
+    return sum([c in "GC" for c in sequence])
+
+def sequencer(record, window_size, step, cur=0):
+    while cur+window_size < len(record.seq):
+        y = 100*gc_content(record.seq[cur:cur+window_size])/window_size
+        x = cur
+        cur += step
+        yield x, y
+
+record = SeqIO.read("fasta_test.fa", "fasta")
+window_size = 4000
+step = 1000
+
+xs = []
+ys = []
+print(f"Expected number of iterations ~{len(record.seq)//step}")
+for x, y in tqdm(sequencer(record, window_size, step)):
+    xs.append(x)
+    ys.append(y)
+
+plt.plot(xs, ys)
+plt.xlim(0, xs[-1])
+plt.ylim(0, 100)
+plt.show()
